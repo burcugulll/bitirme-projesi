dddce1e17805544237db3301eb0dba0e0724f0d6
Burcu <Burcu@macbook-pro-3.local>
2023-11-20 21:43:40 +0300

    yemek tarifleri sayfasında arama yapıldığında bilgiler sayfada gözüküyor

--- lib/widgets/gunluk/yemek-tarifleri-ekrani.dart
+++ lib/widgets/gunluk/yemek-tarifleri-ekrani.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:developer';
 import 'package:http/http.dart';
+
 import 'package:bitirme_projesi/screens/gunluk/model.dart';
 
 class YemekTarifleriPage extends StatefulWidget {
@@ -16,13 +17,18 @@ class _YemekTarifleriPageState extends State<YemekTarifleriPage> {
   getRecipes(String query) async {
     String url =
         'https://api.edamam.com/search?q=$query&app_id=9dbf4610&app_key=0e45552529bb0b4cb1d5c8232226f7ed';
-    Response response = await get(Uri.parse(url));
-    Map data = jsonDecode(response.body);
 
+    Response response = await get(Uri.parse(url));
+    Map<String, dynamic> data = jsonDecode(response.body);
+    List<RecipeModel> updatedRecipeList = [];
     data["hits"].forEach((element) {
       RecipeModel recipeModel = RecipeModel.fromMap(element["recipe"]);
-      recipeList.add(recipeModel);
-      log(recipeList.toString());
+      updatedRecipeList.add(recipeModel);
+      log(updatedRecipeList.toString());
+    });
+
+    setState(() {
+      recipeList = updatedRecipeList;
     });
 
     recipeList.forEach((Recipe) {
@@ -31,6 +37,13 @@ class _YemekTarifleriPageState extends State<YemekTarifleriPage> {
     });
   }
 
+/*
+  @override
+  void initState() {
+    super.initState();
+    //getRecipes("Ladoo");
+  }
+*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,35 +61,101 @@ class _YemekTarifleriPageState extends State<YemekTarifleriPage> {
                   color: Colors.grey[200],
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: TextField(
-                  controller: searchController,
-                  cursorColor: Colors.black,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                      size: 34.0,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: searchController,
+                        cursorColor: Colors.black,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.grey,
+                            size: 34.0,
+                          ),
+                          hintText: 'Yemek Tarifleri Ara',
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
                     ),
-                    hintText: 'Yemek Tarifleri Ara',
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 20.0,
+                    ElevatedButton(
+                      onPressed: () {
+                        if ((searchController.text).replaceAll(" ", "") == "") {
+                          print("Blank search");
+                        } else {
+                          getRecipes(searchController.text);
+                        }
+                      },
+                      child: Text('Tarifleri Ara'),
                     ),
-                  ),
+                  ],
                 ),
               ),
             ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  if ((searchController.text).replaceAll(" ", "") == "") {
-                    print("Blank search");
-                  } else {
-                    getRecipes(searchController.text);
-                  }
+            Container(
+              child: ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: recipeList.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {},
+                    child: Card(
+                      margin: EdgeInsets.all(20),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      elevation: 0.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          ClipRect(
+                            child: Image.network(
+                              recipeList[index].appimgUrl,
+                              fit: BoxFit.cover,
+                              height: 200,
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 10),
+                            color: Colors.black,
+                            child: Text(
+                              recipeList[index].applabel,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 10),
+                            color: Colors.white,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.local_fire_department, size: 13),
+                                Text(
+                                  recipeList[index]
+                                      .appcalories
+                                      .toString()
+                                      .substring(0, 6),
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
                 },
-                child: Text('Tarifleri Ara'),
               ),
             ),
           ],
@@ -85,3 +164,83 @@ class _YemekTarifleriPageState extends State<YemekTarifleriPage> {
     );
   }
 }
+
+
+
+
+
+
+
+/*
+                      child: Stack(
+                        children: [
+                          ClipRect(
+                            child: Image.network(
+                              recipeList[index].appimgUrl,
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                              height: 200,
+                            ),
+                          ),
+                          Positioned(
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 10),
+                              decoration: BoxDecoration(color: Colors.black),
+                              child: Text(
+                                recipeList[index].applabel,
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            right: 0,
+                            width: 40,
+                            height: 80,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(10),
+                                  bottomLeft: Radius.circular(10),
+                                ),
+                              ),
+                              child: Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.local_fire_department, size: 13),
+                                    Text(
+                                      recipeList[index]
+                                          .appcalories
+                                          .toString()
+                                          .substring(0, 6),
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
+
