3dc89a07935b50d4ef4debd6e229765475c8dd08
Burcu <Burcu@MacBook-Pro-3.local>
2023-11-21 14:31:46 +0300

    getRecipes try-catch'e alındı

--- lib/widgets/gunluk/yemek-tarifleri-ekrani.dart
+++ lib/widgets/gunluk/yemek-tarifleri-ekrani.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:developer';
 import 'package:http/http.dart';
-import 'package:bitirme_projesi/widgets/gunluk/recipe-view.dart';
+
 import 'package:bitirme_projesi/screens/gunluk/model.dart';
 
 class YemekTarifleriPage extends StatefulWidget {
@@ -15,26 +15,42 @@ class _YemekTarifleriPageState extends State<YemekTarifleriPage> {
   TextEditingController searchController = TextEditingController();
 
   getRecipes(String query) async {
-    String url =
-        'https://api.edamam.com/search?q=$query&app_id=9dbf4610&app_key=0e45552529bb0b4cb1d5c8232226f7ed';
+    try {
+      String url =
+          'https://api.edamam.com/search?q=$query&app_id=9dbf4610&app_key=0e45552529bb0b4cb1d5c8232226f7ed';
 
-    Response response = await get(Uri.parse(url));
-    Map<String, dynamic> data = jsonDecode(response.body);
-    List<RecipeModel> updatedRecipeList = [];
-    data["hits"].forEach((element) {
-      RecipeModel recipeModel = RecipeModel.fromMap(element["recipe"]);
-      updatedRecipeList.add(recipeModel);
-      log(updatedRecipeList.toString());
-    });
+      Response response = await get(Uri.parse(url));
 
-    setState(() {
-      recipeList = updatedRecipeList;
-    });
+      if (response.statusCode == 200) {
+        dynamic responseData = jsonDecode(response.body);
 
-    recipeList.forEach((Recipe) {
-      print(Recipe.applabel);
-      print(Recipe.appcalories);
-    });
+        if (responseData is Map<String, dynamic> &&
+            responseData.containsKey("hits")) {
+          List<RecipeModel> updatedRecipeList = [];
+
+          responseData["hits"].forEach((element) {
+            RecipeModel recipeModel = RecipeModel.fromMap(element["recipe"]);
+            updatedRecipeList.add(recipeModel);
+            log(updatedRecipeList.toString());
+          });
+
+          setState(() {
+            recipeList = updatedRecipeList;
+          });
+
+          recipeList.forEach((Recipe) {
+            print(Recipe.applabel);
+            print(Recipe.appcalories);
+          });
+        } else {
+          print('Beklenmeyen API yanıt formatı.');
+        }
+      } else {
+        print('HTTP isteği başarısız oldu: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Hata oluştu: $error');
+    }
   }
 
   @override
@@ -170,83 +186,3 @@ class _YemekTarifleriPageState extends State<YemekTarifleriPage> {
     );
   }
 }
-
-
-
-
-
-
-
-/*
-                      child: Stack(
-                        children: [
-                          ClipRect(
-                            child: Image.network(
-                              recipeList[index].appimgUrl,
-                              fit: BoxFit.cover,
-                              width: double.infinity,
-                              height: 200,
-                            ),
-                          ),
-                          Positioned(
-                            left: 0,
-                            right: 0,
-                            bottom: 0,
-                            child: Container(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 5, horizontal: 10),
-                              decoration: BoxDecoration(color: Colors.black),
-                              child: Text(
-                                recipeList[index].applabel,
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            right: 0,
-                            width: 40,
-                            height: 80,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.only(
-                                  topRight: Radius.circular(10),
-                                  bottomLeft: Radius.circular(10),
-                                ),
-                              ),
-                              child: Center(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(Icons.local_fire_department, size: 13),
-                                    Text(
-                                      recipeList[index]
-                                          .appcalories
-                                          .toString()
-                                          .substring(0, 6),
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
-
