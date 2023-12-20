abe5817137001387dc5d655d0416289f2a5da635
Burcu <Burcu@burcu.local>
2023-12-21 01:39:00 +0300

    egzersiz json verileri düzenlendi

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FlutterChat',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
