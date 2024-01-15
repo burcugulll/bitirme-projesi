0e99d3007c29acc834d68e4eec251040a8807189
Burcuu <Burcu@burcu.local>
2024-01-15 22:36:53 +0300

    favoriler kısmı yapıldı

--- lib/screens/gunluk/model.dart
+++ lib/screens/gunluk/model.dart
@@ -4,16 +4,45 @@ class RecipeModel {
   late double appcalories;
   late String appurl;
 
-  RecipeModel(
-      {this.applabel = "LABEL",
-      this.appcalories = 0.000,
-      this.appimgUrl = "IMAGE",
-      this.appurl = "URL"});
-  factory RecipeModel.fromMap(Map recipe) {
+  RecipeModel({
+    this.applabel = "LABEL",
+    this.appcalories = 0.000,
+    this.appimgUrl = "IMAGE",
+    this.appurl = "URL",
+  });
+
+  factory RecipeModel.fromMap(Map? recipe) {
     return RecipeModel(
-        applabel: recipe["label"],
-        appcalories: recipe["calories"],
-        appimgUrl: recipe["image"],
-        appurl: recipe["url"]);
+      applabel: recipe?["label"] ?? "LABEL",
+      appcalories: recipe?["calories"]?.toDouble() ?? 0.000,
+      appimgUrl: recipe?["image"] ?? "IMAGE",
+      appurl: recipe?["url"] ?? "URL",
+    );
+  }
+}
+
+class FBRecipeModel {
+  late String email;
+  late String name;
+  late double calories;
+  late String imageUrl;
+  late String url;
+
+  FBRecipeModel({
+    this.email = "EMAIL",
+    this.name = "LABEL",
+    this.calories = 0.000,
+    this.imageUrl = "IMAGE",
+    this.url = "URL",
+  });
+
+  factory FBRecipeModel.fromMap(Map? data) {
+    return FBRecipeModel(
+      email: data?["email"] ?? "EMAIL",
+      name: data?["name"] ?? "LABEL",
+      calories: data?["calories"]?.toDouble() ?? 0.000,
+      imageUrl: data?["imageUrl"] ?? "IMAGE",
+      url: data?["url"] ?? "URL",
+    );
   }
 }
--- lib/widgets/gunluk/kaydedilen_ogunlar_page.dart
+++ lib/widgets/gunluk/kaydedilen_ogunlar_page.dart
@@ -1,38 +1,164 @@
+import 'package:bitirme_projesi/services/firebase_services.dart';
+import 'package:bitirme_projesi/widgets/web_view_container.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:bitirme_projesi/widgets/gunluk/fav_widget.dart';
 import 'package:bitirme_projesi/screens/gunluk/model.dart';
 
 class KaydedilenOgunlerPage extends StatefulWidget {
-  final List<RecipeModel> recipeList;
-  final Function getFavorites;
-  final List<String> favorites;
-
-  KaydedilenOgunlerPage(
-      {required this.recipeList,
-      required this.getFavorites,
-      required this.favorites});
-
   @override
   _KaydedilenOgunlerPageState createState() => _KaydedilenOgunlerPageState();
 }
 
 class _KaydedilenOgunlerPageState extends State<KaydedilenOgunlerPage> {
+  List<FBRecipeModel> favorites = [];
+  List<String> favoriteNames = [];
+  FirebaseServices firebase = FirebaseServices();
+
+  getFavorites() async {
+    try {
+      // Kullanıcının favori tariflerini Firebase Firestore'dan al
+      final user = await FirebaseAuth.instance.currentUser;
+      print(user?.email);
+
+      // Kullanıcı oturumu açık değilse veya kullanıcı verisi alınamıyorsa işlemi sonlandır
+      if (user == null) {
+        return;
+      }
+
+      final favs =
+          await firebase.favorites.where("email", isEqualTo: user?.email).get();
+
+      if (favs.docs.isEmpty) {
+        return;
+      }
+
+      // Firestore'dan gelen verileri FBRecipeModel türüne dönüştürerek listeye ekle
+      setState(() {
+        favorites = favs.docs
+            .map((doc) => FBRecipeModel.fromMap(doc.data() as Map))
+            .toList();
+
+        favoriteNames = favs.docs.map((doc) => doc['name'] as String).toList();
+      });
+
+      print(favs.docs.length);
+    } catch (error) {
+      print('Favori tarifleri getirirken hata oluştu: $error');
+      // Hata durumunda boş bir liste döndür
+      return [];
+    }
+  }
+
   @override
   void initState() {
     super.initState();
-    // Favori tarifleri getirmek için ilgili fonksiyonu çağır
-    widget.getFavorites();
+
+    getFavorites();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Kaydedilen Öğünler Sayfası'),
+        title: Text('Favoriler Sayfası'),
       ),
-      body: Center(
-        child: Text('Burada kaydedilen öğünler içeriği bulunabilir.'),
-      ),
+      body: favorites.isEmpty
+          ? Center(
+              child: Text('Henüz kaydedilen öğün bulunmamaktadır.'),
+            )
+          : Container(
+              child: ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: favorites.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => WebViewContainer(
+                            url: favorites[index].url,
+                            recipe: favorites[index].name,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      margin: EdgeInsets.all(20),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      elevation: 0.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          ClipRect(
+                            child: Image.network(
+                              favorites[index].imageUrl,
+                              fit: BoxFit.cover,
+                              height: 200,
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 10),
+                            color: Colors.black,
+                            child: Text(
+                              favorites[index].name,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 10),
+                            color: Colors.white,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(Icons.local_fire_department, size: 20),
+                                    SizedBox(
+                                      width: 2,
+                                    ),
+                                    Text(
+                                      favorites[index]
+                                              .calories
+                                              .round()
+                                              .toString() +
+                                          " cal",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                FavoriKalp(
+                                  recipe: RecipeModel(
+                                      appurl: favorites[index].url,
+                                      appcalories: favorites[index].calories,
+                                      appimgUrl: favorites[index].imageUrl,
+                                      applabel: favorites[index].name),
+                                  isFavorited: favoriteNames
+                                      .contains(favorites[index].name),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
       backgroundColor: Colors.grey[200],
     );
   }
